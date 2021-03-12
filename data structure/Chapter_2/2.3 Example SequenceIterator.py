01e6d11cc16ede03b65bb07899b0ccbd537ed70c
Lucy Wang <woshilucy712@gmail.com>
2021-03-12 12:29:58 -0600

    Mar 12 2021

---
+++ data structure/Chapter_2/2.3 Example SequenceIterator.py
@@ -0,0 +1,29 @@
+
+### write a class with an given data sequence. Each time__next__ is called, 
+### the index is incremented, until reaching the end of sequence 
+
+
+class SequenceIterator():
+
+    def __init__(self,data):
+        self._data = data 
+        self._index = -1 
+    
+    def __len__(self):
+        return len(self._data)
+    
+    def __getitem__(self,i):
+        return self._data[i]
+    
+    def __next__(self):
+        self._index +=1 
+        if self._index < len(self._data):
+            return self._data[self._index]
+        else:
+            raise StopIteration()
+    
+    def __iter__(self):
+        '''By convention, an iterator must return itself as an iterator'''
+        return slef 
+
+
